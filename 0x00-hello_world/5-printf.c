9b764872c15124eec4a75cf92e23a09eff0881e4
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-08-10 21:07:29 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * This is the main block
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-/*This is the printf function*/
+/*This is the main function*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
