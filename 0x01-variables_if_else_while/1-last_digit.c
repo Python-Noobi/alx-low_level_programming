ac0656f1fe57f1d35c9f2e7754ae49cabc53701e
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-08-11 21:55:10 +0300

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: get a number then compare it with 5.
  * Return: 0
  */
 int main(void)
