734a078e61458ec49c973bf622efc0c103addd38
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-08-11 22:32:34 +0300

    Task 8

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,14 +9,10 @@
  */
 int main(void)
 {
-	char c = 'z';
-
-	while (c >= 'a')
-	{
-		putchar(c);
-		c--;
+	for (char c = 'z'; c >= 'a'; c--) {
+	  putchar(c);
 	}
-
-	putchar('\n');
+	
+	printf("\n");
 	return (0);
 }
