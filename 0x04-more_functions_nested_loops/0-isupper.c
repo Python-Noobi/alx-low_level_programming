a531b7c3de38e8c49c071b7b581ee9540b707c83
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-08-17 23:28:18 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks if the character is uppercase
+ * _isupper - checks if the character is uppercase ---
  * @c:  is the int that will use for the argument of the function
  * Return: 1 if uppercase character 0 if not
  */
