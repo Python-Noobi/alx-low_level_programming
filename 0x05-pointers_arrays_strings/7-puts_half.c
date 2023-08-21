bb6498ded8e061f1982572e5b83ad83acbd1054a
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-08-21 21:54:13 +0300

    File 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts_half - prints a string, followed by a new line,
+ * puts_half - prints a string, followed by a new line, ---
  * @str: pointer to the string to print
  * Return: void
 */
