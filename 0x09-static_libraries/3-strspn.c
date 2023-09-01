fd59373d7f162fc03d7b4ddd808dd7002bb45338
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-09-01 22:34:05 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strspn - Gets the length of a prefix substring.
+* _strspn - Gets the length of a prefix substring.-
 * @s: String where substring will look.
 * @accept: Substring of accepted chars.
 * Return: Length of occurrence.
