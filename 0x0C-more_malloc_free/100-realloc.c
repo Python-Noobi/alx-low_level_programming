8c1f7e63ed46f706249709146ef5c7bb35ed83dd
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-09-07 19:16:15 +0300

    Task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _realloc -  reallocates a memory block using malloc and free
+ * _realloc -  reallocates a memory block using malloc and free-
  * @ptr: pointer
  * @old_size: old size
  * @new_size: new size
