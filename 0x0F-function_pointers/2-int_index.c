37842fc154e5798113f3301e03bbc2ab0e913f8e
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-09-14 21:31:38 +0300

    Tasks 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
+ * int_index - searches for an integer-
  * @array: array of elemnts
  * @size: number of elements in the array
  * @cmp: is a pointer to the function to be used to compare values
