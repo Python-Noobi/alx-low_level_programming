80e7a2cda76f2d2c7d91072fe43ae4dac4e65936
@PythonNoobi <47674351+Python-Noobi@users.noreply.github.com>
2023-09-14 21:31:00 +0300

    Tasks 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * array_iterator - executes funct
+ * array_iterator - executes funct-
  * @array: array of elements
  * @size: array's size
  * @action: pointer to function
